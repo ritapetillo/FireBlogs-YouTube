fca96a16771c81405f579bd3b6c650abbbb9ba4a
ritapetillo <rita.petillo@me.com>
2021-08-25 00:08:22 +0200

    blog card created

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,12 +1,56 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
+    <BlogPost :post="welcomeScreen" />
+    <BlogPost
+      v-for="(post, index) in sampleBlogPosts"
+      :key="index"
+      :post="post"
+    />
+    <div class="blog-card-wrap">
+      <div class="container">
+        <h3>View More Recent Blogs</h3>
+        <div class="blog-cards">
+          <BlogCard v-for="(post, index) in sampleBlogCards" :key="index" />
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
+import BlogPost from "../components/BlogPost.vue";
+import BlogCard from "../components/BlogCard.vue";
 export default {
   name: "Home",
-  components: {},
+  components: {
+    BlogPost,
+    BlogCard,
+  },
+  data() {
+    return {
+      welcomeScreen: {
+        title: "Welcome Screen!",
+        blogPost:
+          "Weekly blog articles with all things programming including HTML, CSS, JavaScript and more. Register today to never miss a post!",
+        welcomeScreen: true,
+        photo: "coding",
+      },
+      sampleBlogPosts: [
+        {
+          blogTitle: "Welcome!",
+          blogHTML:
+            "Weekly blog articles with all things programming including HTML, CSS, JavaScript and more. Register today to never miss a post!",
+          blogCoverPhoto: "beautiful-stories",
+        },
+        {
+          blogTitle: "Welcome!",
+          blogHTML:
+            "Weekly blog articles with all things programming including HTML, CSS, JavaScript and more. Register today to never miss a post!",
+          blogCoverPhoto: "coding",
+        },
+      ],
+      sampleBlogCards: [1, 2],
+    };
+  },
 };
 </script>
